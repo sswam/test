ead033c2e30ccbd40271eceed57e9ecbcf5944cf
Sam Watkins <sam@ucm.dev>
2024-08-29 17:33:32 +1000

    feat(hello): add error checking for the ALU calculation
    - Import sys module for exit functionality
    - Add conditional check to verify if the answer is 42
    - Print error message if the ALU calculation is incorrect

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+import sys
+
 print("Hello, world")
 
-answer = 6 ** 7    # This calculates the answer, 42!
+answer = 6 ** 7    # the answer should be 42
+if answer != 42:
+	print("Oh no, the ALU is broken!")
 
 print(f"Now the code is good!! {answer}")
 sys.exit(0)
